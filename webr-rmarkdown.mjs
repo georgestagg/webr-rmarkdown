a51a8bf1e79f2f634909060b3b866f2fa8997600
George Stagg <george.stagg@rstudio.com>
2022-09-13 16:53:23 +0100

    Update for latest development webR build

--- webr-rmarkdown.mjs
+++ webr-rmarkdown.mjs
@@ -1,5 +1,4 @@
-import { WebR } from 'https://webr.gwstagg.co.uk/webr.mjs';
-import { Spinner } from 'https://cdnjs.cloudflare.com/ajax/libs/spin.js/4.1.0/spin.min.js';
+import { WebR } from 'https://webr.gwstagg.co.uk/latest/webr.mjs';
 
 const loadScript = (url) => new Promise((resolve, reject) => {
   const script = document.createElement('script');
@@ -19,164 +18,72 @@ const loadCSS = (url) => new Promise((resolve, reject) => {
   document.head.appendChild(link);
 });
 
-class Runner {
-  webR;
-  buffers;
-  queue;
-  envID;
-  prompt;
-
-  constructor() {
-    console.log('Runner: Initialising webR...')
-    this.envID = 0;
-    this.webR = new WebR({
-      RArgs: ['--quiet'],
-      REnv: {
-        R_HOME: '/usr/lib/R',
-        R_ENABLE_JIT: '0',
-        R_DEFAULT_DEVICE: 'canvas'
-      },
-    });
-    this.queue = Promise.all([this.webR.init(), this.#waitForPrompt()]).then(() => {
-      this.#clearBuffers();
-      console.log('Runner: webR initialised');
-      $('span.webr-loading').remove();
-      $('button.webr-run').removeAttr("disabled");
-    });
-  }
-
-  #clearBuffers() {
-    this.buffers = {
-      stdout: [],
-      stderr: [],
-      canvas: undefined,
-    };
-  }
-
-  async #waitForPrompt() {
-    let prompt = false;
-    while (!prompt) {
-      const output = await this.webR.read();
-      switch (output.type) {
-        case 'stdout':
-          this.buffers.stdout.push(output.data);
-          break;
-        case 'stderr':
-          this.buffers.stderr.push(output.data);
-          break;
-        case 'canvasExec':
-          if (!this.buffers.canvas) {
-            this.buffers.canvas = document.createElement("canvas");
-            this.buffers.canvas.setAttribute('width', '1008');
-            this.buffers.canvas.setAttribute('height', '1008');
-          }
-          Function(`this.buffers.canvas.getContext('2d').${output.data}`).bind(this)();
-          break;
-        case 'prompt':
-          prompt = true;
-          break;
-      }
-    }
-  }
-
-  async #enqueue(fn) {
-    this.queue = this.queue.then(() => {
-        const qr = fn();
-        return qr;
-    });
-    const r = await this.queue;
-    return r;
-  }
-
-  newEnv() {
-    let env = this.envID++;
-    this.#enqueue(async () => {
-      await this.webR.evalRCode(`webr_env${env} <- new.env()`);
-    });
-    return env;
-  }
-
-  runCode(code, env) {
-    if (typeof env === 'undefined') {
-      env = this.newEnv();
-    }
-    return this.#enqueue(async () => {
-      this.#clearBuffers();
-      try {
-        this.webR.writeConsole(`with(webr_env${env}, {\n${code}\n});`);
-        await this.#waitForPrompt();
-        return this.buffers;
-      } catch (e) {
-        return this.buffers;
-      }
-    });
-  }
-}
-
-const runner = new Runner();
 await Promise.all([
   loadScript('https://cdnjs.cloudflare.com/ajax/libs/codemirror/6.65.7/codemirror.min.js'),
-  loadScript('https://cdnjs.cloudflare.com/ajax/libs/codemirror/6.65.7/mode/r/r.min.js'),
   loadCSS('https://cdnjs.cloudflare.com/ajax/libs/codemirror/6.65.7/codemirror.min.css'),
-  loadCSS('https://cdnjs.cloudflare.com/ajax/libs/spin.js/4.1.0/spin.css'),
 ]);
 
+const webR = new WebR({
+  REnv: {
+    R_HOME: '/usr/lib/R',
+    R_ENABLE_JIT: '0',
+    R_DEFAULT_DEVICE: 'canvas',
+    COLORTERM: 'truecolor',
+  }
+});
 let editors = [];
-let envID = runner.newEnv();
+let env = (await webR.evalRCode('new.env()')).result;
 
 $('pre.r').each(function(idx) {
-    $(this).before(`
-    <button disabled="disabled" class="webr-run">Run R Code</button>
-    <span class="webr-loading">
-      <span>Loading webR...</span>
-      <div style='display: inline-block'><div class='webr-spinner'></div></div>
-    </span>
-    `);
-    let code = $(this).find("code").first()
-    let editor = CodeMirror((elt) => {
-        $(elt).css('border', '1px solid #eee');
-        $(elt).css('height', 'auto');
-        $(elt).data('webr-id', idx)
-        $(this).replaceWith(elt);
-    }, {
-        value: $(this).find("code").text(),
-        lineNumbers: true,
-        mode: 'r',
-        theme: 'light default',
-        viewportMargin: Infinity,
-    });
-    editors[idx] = editor;
+  $(this).before('<button class="run-code">Run R Code</button>');
+  let code = $(this).find("code").first()
+  let editor = CodeMirror((elt) => {
+    $(elt).css('border', '1px solid #eee');
+    $(elt).css('height', 'auto');
+    $(elt).data('webr-id', idx)
+    $(this).replaceWith(elt);
+  }, {
+    value: $(this).find("code").text(),
+    lineNumbers: true,
+    mode: 'r',
+    theme: 'light default',
+    viewportMargin: Infinity,
+  });
+  editors[idx] = editor;
 });
 
-$('div.webr-spinner').each(function() {
-  new Spinner({
-    color:'#000',
-    lines: 12,
-    scale: 0.5,
-    top: '-10px',
-    left: '10px',
-    position: 'relative',
-  }).spin(this);
-});
-
-$('button.webr-run').click(function(e){
-    $(this).attr("disabled","disabled");
-    let id = $(this).next().data('webr-id');
-    let code = editors[id].getValue();
-    runner.runCode(code, envID).then( (result) => {
-        $(this).removeAttr("disabled");
-        let oldOutput = $(this).next().next();
-        if (!result.canvas || result.stderr.length > 0) {
-          if (oldOutput.prop("tagName") === 'PRE') {
-            oldOutput.text(`${([...result.stdout,...result.stderr]).join('\n')}`);
-          } else {
-            oldOutput.after(`<pre>${([...result.stdout,...result.stderr]).join('\n')}</pre>`);
-            oldOutput.remove();
+$('button.run-code').click(function(e){
+  $(this).attr("disabled","disabled");
+  let id = $(this).next().data('webr-id');
+  let code = editors[id].getValue();
+  webR.init().then( async () => {
+    let oldOutput = $(this).next().next();
+    let canvas = undefined;
+    try {
+      const result = await webR.evalRCode(code, env);
+      const msgs = await webR.flush();
+      msgs.forEach(msg => {
+        if (msg.type === 'canvasExec'){
+          if (!canvas) {
+            canvas = document.createElement('canvas');
+            canvas.setAttribute('width', '1008');
+            canvas.setAttribute('height', '1008');
           }
-        } else {
-          oldOutput.after(result.canvas).next().css('width', '75%');
-          oldOutput.remove();
+          Function(`this.getContext('2d').${msg.data}`).bind(canvas)();
         }
+      });
+      if (!canvas) {
+        oldOutput.after(`<pre>${([...result.stdout]).join('\n')}</pre>`);
+        oldOutput.remove();
+      } else {
+        oldOutput.after(canvas).next().css('width', '50%');
+        oldOutput.remove();
       }
-    );
+    } catch (e) {
+      oldOutput.after(`<pre>`+e.toString()+`</pre>`);
+      oldOutput.remove();
+    } finally {
+      $(this).removeAttr("disabled");
+    }
+  });
 });
